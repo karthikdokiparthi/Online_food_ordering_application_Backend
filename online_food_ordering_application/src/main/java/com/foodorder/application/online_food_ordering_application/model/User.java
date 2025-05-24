24f4fc610419308a6d267f01c46a791c294329d4
karthikdokiparthik <dokiparthikarthik@gmail.com>
2025-05-24 12:26:44 +0530

    feat(user): integrate role-based access by adding UserRole enum to User entity with default ROLE_USER

--- online_food_ordering_application/src/main/java/com/foodorder/application/online_food_ordering_application/model/User.java
+++ online_food_ordering_application/src/main/java/com/foodorder/application/online_food_ordering_application/model/User.java
@@ -21,7 +21,7 @@ public class User {
     private String lastName;
 
     @Column(unique = true, nullable = false)
-    private String username; // Auto-generated from firstName + lastName
+    private String username;
 
     @Column(unique = true, nullable = false)
     @Email(message = "Invalid email format")
@@ -30,11 +30,15 @@ public class User {
     @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role = UserRole.ROLE_USER;
+
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     @JsonIgnore
     private Cart cart;
 
-    @OneToOne(mappedBy = "user",cascade = CascadeType.ALL)
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     @JsonIgnore
     private UserAddress userAddress;
 }
