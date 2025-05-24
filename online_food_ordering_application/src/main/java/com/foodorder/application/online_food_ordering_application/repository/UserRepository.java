068aba93df74295a35c195975ee0f4548d23e968
karthikdokiparthik <dokiparthikarthik@gmail.com>
2025-05-24 12:22:02 +0530

    feat(user): add existsByUsername method to support user registration validation

--- online_food_ordering_application/src/main/java/com/foodorder/application/online_food_ordering_application/repository/UserRepository.java
+++ online_food_ordering_application/src/main/java/com/foodorder/application/online_food_ordering_application/repository/UserRepository.java
@@ -11,4 +11,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     int countByUsernameStartingWith(String prefix);
 
     boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
 }
